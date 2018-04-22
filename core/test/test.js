64f769080a6636bc5f0247d0d4ac0bf9a074fd4e
Adarsh Iyer <adarsh.iyer17@gmail.com>
2018-04-22 09:05:12 -0700

    package-lock.json

--- core/test/test.js
+++ core/test/test.js
@@ -1,1 +1,22 @@
-!function(e){var o={};function n(r){if(o[r])return o[r].exports;var t=o[r]={i:r,l:!1,exports:{}};return e[r].call(t.exports,t,t.exports,n),t.l=!0,t.exports}n.m=e,n.c=o,n.d=function(e,o,r){n.o(e,o)||Object.defineProperty(e,o,{configurable:!1,enumerable:!0,get:r})},n.r=function(e){Object.defineProperty(e,"__esModule",{value:!0})},n.n=function(e){var o=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(o,"a",o),o},n.o=function(e,o){return Object.prototype.hasOwnProperty.call(e,o)},n.p="",n(n.s=0)}([function(e,o){var n;console.log("hey"),n="hello",console.log(n);for(var r=0,t=0;t<=10;t++)r=r+t;console.log(r)}]);
+var myFunc = function() {
+console.log("hey");
+}
+var thisFunc = function(x) {
+console.log(x);
+}
+var a = "hello";
+myFunc();
+thisFunc(a);
+var b = "four";
+var if = is "three";
+console.log(3);
+}
+var else = b is "five";
+console.log(5);
+console.log(4);
+}
+var sum = 0;
+for (var i = 0; i<=10;i++) {
+var sum = sum+i;
+}
+console.log(sum);
