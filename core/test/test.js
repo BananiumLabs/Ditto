d99ceff37da8327130847e90768e5335d60e98cd
Vikram Peddinti <vikram.peddinti@gmail.com>
2018-04-22 00:35:44 -0700

    If Else Statements

--- core/test/test.js
+++ core/test/test.js
@@ -1,11 +1,17 @@
-var test = 10;
+var test = 11;
 var name = function(x) {
-var testq2 = 21940;
-var test9 = 2;
-console.log(test9);
+var testq2 = 21940;
+var test9 = 2;
+console.log(test9);
 }
-var food = 'good';
+var food = 'good';
+if (test==10) {
+console.log("If Called");
+}
+else {
+console.log("Else Called");
+}
 for (var i = 0; i<10;i++) {
-console.log(i);
+console.log(i);
 }
 name("hi");
